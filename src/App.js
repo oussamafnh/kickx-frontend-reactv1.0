a144e3d721d5368ca5cbd758bf97df8e54e38a1d
oussama <fannahoussama4@gmail.com>
2023-11-20 11:41:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Nav from "./coomponents/nav";
+import "./coomponents/css/app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <div className="ccc">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
